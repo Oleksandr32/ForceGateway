14651d78746f1c56394f8c191f51568ed7861f80
Oleksandr Lysun <oleksandr.lysun@teamvoy.com>
2019-02-06 17:27:15 +0200

    #4: Home: add Home activity

--- app/src/main/java/com/oleksandrlysun/forcegateway/presentation/MainActivity.kt
+++ app/src/main/java/com/oleksandrlysun/forcegateway/presentation/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.oleksandrlysun.forcegateway.presentation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.oleksandrlysun.forcegateway.presentation.screens.home.HomeActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        startActivity(Intent(this, HomeActivity::class.java))
+        finish()
     }
 }
