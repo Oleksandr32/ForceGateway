850bc29451f4aa795570f5852c071e001561e381
Oleksandr Lysun <oleksandr.lysun@teamvoy.com>
2019-02-07 18:30:32 +0200

    Home: add extension for simpler start activity

--- app/src/main/java/com/oleksandrlysun/forcegateway/extension/AndroidExtension.kt
+++ app/src/main/java/com/oleksandrlysun/forcegateway/extension/AndroidExtension.kt
@@ -1,11 +1,20 @@
 package com.oleksandrlysun.forcegateway.extension
 
 import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
 import android.view.View
 import androidx.annotation.IdRes
 import com.oleksandrlysun.forcegateway.utils.lazyUnsynchronized
 
-fun <ViewT: View> Activity.bindView(@IdRes idRes: Int): Lazy<ViewT> {
+inline fun <reified T : Activity> Context.startActivity(bundle: Bundle? = null, buildIntent: Intent.() -> Unit = {}) {
+	val intent = Intent(this, T::class.java)
+	intent.buildIntent()
+	startActivity(intent, bundle)
+}
+
+fun <ViewT : View> Activity.bindView(@IdRes idRes: Int): Lazy<ViewT> {
 	return lazyUnsynchronized {
 		findViewById<ViewT>(idRes)
 	}
