7bc6389e5844457db4fc4edcc4bbb0e179de01af
Oleksandr Lysun <oleksandr.lysun@teamvoy.com>
2019-02-18 14:42:17 +0200

    5. Encrypt: add Encrypt activity

---
+++ app/src/main/java/com/oleksandrlysun/forcegateway/presentation/screens/encrypt/EncryptActivity.kt
@@ -0,0 +1,13 @@
+package com.oleksandrlysun.forcegateway.presentation.screens.encrypt
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.oleksandrlysun.forcegateway.R
+
+class EncryptActivity : AppCompatActivity() {
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_encrypt)
+	}
+}
